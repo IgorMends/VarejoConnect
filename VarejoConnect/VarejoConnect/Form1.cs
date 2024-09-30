dea90430cfa89d0abd922ccd826f4ea5581c51f9
Igor Mendes Gomes <146093563+IgorMends@users.noreply.github.com>
2024-09-30 12:56:37 -0300

    Login Layout
    Neste commit está apenas o Layout da pagina de login,

---
+++ VarejoConnect/VarejoConnect/Form1.cs
@@ -0,0 +1,25 @@
+namespace VarejoConnect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
